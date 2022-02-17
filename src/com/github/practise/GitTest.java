4fa946bb1ee032e6d8cd70cebefa4d62eb315dd0
Deepak Prabhu <deepakjugudu94@gmail.com>
2022-02-17 16:05:12 +0530

    First Commit

---
+++ src/com/github/practise/GitTest.java
@@ -0,0 +1,12 @@
+package com.github.practise;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is First Github Project I am using now");
+		
+		System.out.println("Deepak is expert in selenium");
+		
+	}
+}
---
+++ src/com/github/practise/Gittest_2.java
@@ -0,0 +1,12 @@
+package com.github.practise;
+
+public class Gittest_2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Deepak Prabhu is learning GitBash Command");
+
+		System.out.println("Deepak is expert in selenium");
+	}
+
+}
